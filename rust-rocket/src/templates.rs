552fa2b783547fd0d3083fddc6cf53a8117efb7e
Raboro <raborogit@gmail.com>
2024-07-02 00:31:32 +0200

    feat(Todos): :sparkles: started template for todos

--- rust-rocket/src/templates.rs
+++ rust-rocket/src/templates.rs
@@ -3,3 +3,9 @@ use askama::Template;
 #[derive(Template)]
 #[template(path = "index.html")]
 pub struct IndexTemplate;
+
+#[derive(Template)]
+#[template(path = "todos.html")]
+pub struct TodosTemplate<'a> {
+    pub todos: &'a Vec<String>
+}
