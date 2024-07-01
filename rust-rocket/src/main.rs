53a8c83df59b5b758b00ec87e65ff86ce177a756
Raboro <raborogit@gmail.com>
2024-07-02 00:32:15 +0200

    feat(Todos): :sparkles: created Todos State and added endpoint to fetch todos

--- rust-rocket/src/main.rs
+++ rust-rocket/src/main.rs
@@ -1,17 +1,27 @@
 #[macro_use]
 extern crate rocket;
-use rocket::fs::FileServer;
+use rocket::{fs::FileServer, State};
 
 mod templates;
+
+struct Todos {
+    todos: Vec<String>
+}
 
 #[get("/")]
 fn index() -> templates::IndexTemplate {
     templates::IndexTemplate {}
 }
 
+#[get("/todos")]
+fn get_todos(todos: &State<Todos>) -> templates::TodosTemplate {
+    templates::TodosTemplate { todos: &todos.todos }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index])
+        .mount("/", routes![index, get_todos])
         .mount("/static", FileServer::from("static"))
+        .manage(Todos {todos: vec!["Shower".to_string(), "Exercise".to_string()]})
 }
