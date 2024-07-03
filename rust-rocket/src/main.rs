df9672854d0b6d2da3a7ee2dbb81c9572e8c19d9
Raboro <raborogit@gmail.com>
2024-07-04 00:19:31 +0200

    feat(REST): :sparkles: added TodoForm and post endpoint

--- rust-rocket/src/main.rs
+++ rust-rocket/src/main.rs
@@ -1,11 +1,17 @@
 #[macro_use]
 extern crate rocket;
-use rocket::{fs::FileServer, State};
+use rocket::{form::Form, fs::FileServer, State};
 
 mod templates;
 
 struct Todos {
     todos: Vec<String>,
+}
+
+#[derive(FromForm)]
+struct TodoForm<'a> {
+    #[field(validate = len(1..))]
+    todo: &'a str
 }
 
 #[get("/")]
@@ -20,10 +26,15 @@ fn get_todos(todos: &State<Todos>) -> templates::TodosTemplate {
     }
 }
 
+#[post("/todo", data = "<todo_form>")]
+fn post_todo(todo_form: Form<TodoForm<'_>>) {
+    println!("{}", todo_form.todo);
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, get_todos])
+        .mount("/", routes![index, get_todos, post_todo])
         .mount("/static", FileServer::from("static"))
         .manage(Todos {
             todos: vec!["Shower".to_string(), "Exercise".to_string()],
