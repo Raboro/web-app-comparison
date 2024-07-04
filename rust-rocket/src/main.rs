6ce865f297872caade7b8050a7dfde8146480231
Raboro <raborogit@gmail.com>
2024-07-04 18:25:23 +0200

    feat(Todo): :sparkles: added delete endpoint for todo

--- rust-rocket/src/main.rs
+++ rust-rocket/src/main.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate rocket;
 use core::fmt;
-use std::sync::RwLock;
+use std::{str::FromStr, sync::RwLock};
 
 use rocket::{form::Form, fs::FileServer, State};
 use uuid::Uuid;
@@ -58,10 +58,23 @@ fn post_todo(todo_form: Form<TodoForm<'_>>, todos: &State<Todos>) -> templates::
     }
 }
 
+#[delete("/todo/<id>")]
+fn delete_todo(id: &str, todos: &State<Todos>) -> templates::TodosTemplate{
+    let uuid = Uuid::from_str(id);
+    if uuid.is_err() {
+        return templates::TodosTemplate {
+            todos: todos.todos.write().unwrap().clone()
+        };
+    }
+    let mut todos_guard = todos.todos.write().unwrap();
+    todos_guard.retain(|todo| todo.id != uuid.clone().unwrap());
+    templates::TodosTemplate { todos: todos_guard.clone() }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", routes![index, get_todos, post_todo])
+        .mount("/", routes![index, get_todos, post_todo, delete_todo])
         .mount("/static", FileServer::from("static"))
         .manage(Todos {
             todos: RwLock::new(vec![Todo::new("Shower".to_string()), Todo::new("Exercise".to_string())]),
